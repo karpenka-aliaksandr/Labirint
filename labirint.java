8a917f0b92ac3d87ea091cc8dfcec0023cbf3599
karpenkaaliaksandr <kasasha@mail.ru>
2023-02-23 21:00:37 +0300

    добавил createArray

--- labirint.java
+++ labirint.java
@@ -6,7 +6,16 @@ public class labirint {
     public static void main(String[] args) {
         int M = 10;
         int N = 10;
-
+        int[][] matrix = createArray(M, N);
+        fillArray(matrix);
 
     }
+    public static int[][] createArray(int m, int n){
+        return new int[m][n];
+    }
+
+    // static void fillArray(int[][] arr){
+    //     for arr.
+    // };
+
 }
