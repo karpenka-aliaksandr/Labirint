00cf5dab64ed536ff818c1a5dcf0cfc96aafe2b5
Nikita_Lyskov <nikitam.l@mail.ru>
2023-02-23 21:00:36 +0300

    add new method printarray

--- labirint.java
+++ labirint.java
@@ -6,7 +6,16 @@ public class labirint {
     public static void main(String[] args) {
         int M = 10;
         int N = 10;
+        int[][] testArray = new int[3][3];
+        printArray(testArray);
+    }
 
-
+    public static void printArray(int[][] array) {
+        for (int[] line : array){
+            for (int item: line){
+                System.out.printf("%d", item);
+            }
+            System.out.println();
+        }
     }
 }
